6c27ae59286796170e93ddd13c0d99de5015724f
zimice <simon.kochanek@me.com>
2020-12-19 08:17:48 +0100

    Finished work for morning

--- VisualProject/Cpp_6502/Cpp_6502/main.cpp
+++ VisualProject/Cpp_6502/Cpp_6502/main.cpp
@@ -8,102 +8,108 @@ using u32 = unsigned int;
 
 struct Mem
 {
-    static constexpr u32 MAX_MEM = 1024 * 64;
-    Byte Data[MAX_MEM];
+	static constexpr u32 MAX_MEM = 1024 * 64;
+	Byte Data[MAX_MEM];
 
-    void Initialise()
-    {
-        for (u32 i = 0; i < MAX_MEM; i++)
-        {
-            Data[i] = 0;
-        }
-    }
-    //** read 1 byte **//
-    Byte operator[](u32 Address) const
-    {
-        //assert here Address is < MAX_MEM
-        return Data[Address];
-    }
-    //** write 1 byte **//
-    Byte& operator[](u32 Address) 
-    {
-        //assert here Address is < MAX_MEM
-       return Data[Address];
-    }
+	void Initialise()
+	{
+		for (u32 i = 0; i < MAX_MEM; i++)
+		{
+			Data[i] = 0;
+		}
+	}
+	//** read 1 byte **//
+	Byte operator[](u32 Address) const
+	{
+		//assert here Address is < MAX_MEM
+		return Data[Address];
+	}
+	//** write 1 byte **//
+	Byte& operator[](u32 Address)
+	{
+		//assert here Address is < MAX_MEM
+		return Data[Address];
+	}
 
 };
 
 struct CPU
 {
 
-    Word PC; //Program Counter
-    Word SP; //Stack pointer
+	Word PC; //Program Counter
+	Word SP; //Stack pointer
 
-    Byte A, X, Y; // Registers
+	Byte A, X, Y; // Registers
 
-    Byte C : 1; //Status flag
-    Byte Z : 1;
-    Byte I : 1;
-    Byte D : 1;
-    Byte B : 1;
-    Byte V : 1;
-    Byte N : 1;
+	Byte C : 1; //Status flag
+	Byte Z : 1;
+	Byte I : 1;
+	Byte D : 1;
+	Byte B : 1;
+	Byte V : 1;
+	Byte N : 1;
 
-    void Reset(Mem &memory)
-    {
-        PC = 0xFFFC;
-        SP = 0x100;
-        C = Z = I = D = B = V = N = D = 0;
-        A = X = Y = 0;
-        memory.Initialise();
-    }
+	void Reset(Mem& memory)
+	{
+		PC = 0xFFFC;
+		SP = 0x100;
+		C = Z = I = D = B = V = N = D = 0;
+		A = X = Y = 0;
+		memory.Initialise();
+	}
 
-    Byte FetchByte( u32& Cycles, Mem &memory)
-    {
-        Byte Data = memory[PC];
-        PC++;
-        Cycles--;
-        return Data;
-    }
-    //opcodes
-    static constexpr Byte
-        INS_LDA_IM = 0xA9;
+	Byte FetchByte(u32& Cycles, Mem& memory)
+	{
+		Byte Data = memory[PC];
+		PC++;
+		Cycles--;
+		return Data;
+	}
+	//opcodes
+	static constexpr Byte
+		INS_LDA_IM = 0xA9,
+		INS_LDA_ZP = 0XA5;
+		
 
-    void Execute(u32 Cycles, Mem& memory)
-    {
-        while (Cycles > 0)
-        {
-            Byte Ins = FetchByte(Cycles, memory);
-            switch (Ins)
-            {
-            case INS_LDA_IM:
-            {
-                Byte Value = FetchByte(Cycles, memory);
-                A = Value;
-                Z = (A == 0);
-                N = (A & 0b10000000) > 0;
-            }
-            break;
-            default:
-            {
-                printf("Instruction not handled %d", Ins);
-            }
-            break;
-            }
-        }
-    }
+	void Execute(u32 Cycles, Mem& memory)
+	{
+		while (Cycles > 0)
+		{
+			Byte Ins = FetchByte(Cycles, memory);
+			switch (Ins)
+			{
+			case INS_LDA_IM:
+			{
+				Byte Value = FetchByte(Cycles, memory);
+				A = Value;
+				Z = (A == 0);
+				N = (A & 0b10000000) > 0;
+			}
+			case INS_LDA_ZP:
+			{
+
+			}
+			break;
+			default:
+			{
+				printf("Instruction not handled %d", Ins);
+			}
+			break;
+			}
+		}
+	}
 };
 
 int main()
 {
-    Mem mem;
-    CPU cpu;
-    cpu.Reset(mem);
-    //inline little program
-    mem[0xFFFC] = CPU::INS_LDA_IM;
-    mem[0xFFFD] = 0x42;
-    //inline little program
-    cpu.Execute(2, mem); //TODO Need to find suitable debugger
-    printf("Program Finished");
-    return 0;
+	Mem mem;
+	CPU cpu;
+	cpu.Reset(mem);
+	//inline little program
+	mem[0xFFFC] = CPU::INS_LDA_IM;
+	mem[0xFFFD] = 0x42;
+	//inline little program
+	cpu.Execute(2, mem); //TODO Need to find suitable debugger
+	printf("Program Finished");
+	return 0;
 }
