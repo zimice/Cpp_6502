7802168e9eb8e286000ef1c4ae10b642bf299359
zimice <simon.kochanek@me.com>
2021-01-05 08:24:29 +0100

    45 minute

--- main.cpp
+++ main.cpp
@@ -30,106 +30,141 @@ struct Mem
 		//assert here Address is < MAX_MEM
 		return Data[Address];
 	}
-};
-
-struct CPU
-{
-
-	Word PC; //Program Counter
-	Word SP; //Stack pointer
-
-	Byte A, X, Y; // Registers
-
-	Byte C : 1; //Status flag
-	Byte Z : 1;
-	Byte I : 1;
-	Byte D : 1;
-	Byte B : 1;
-	Byte V : 1;
-	Byte N : 1;
-
-	void Reset(Mem &memory)
+	//** write 2 bytes **//
+	void WriteWord(u32 Cycles,Word Value, u32 Address)
 	{
-		PC = 0xFFFC;
-		SP = 0x100;
-		C = Z = I = D = B = V = N = D = 0;
-		A = X = Y = 0;
-		memory.Initialise();
-	}
+		Data[Address] = Value & 0xFF;
+		Data[Address+1] = (Value >> 8 );
+		Cycles -=2;
 
-	Byte FetchByte(u32& Cycles, Mem &memory)
-	{
-		Byte Data = memory[PC];
-		PC++;
-		Cycles--;
-		return Data;
-	}
-	Byte ReadByte(u32 &Cycles,Byte Address, Mem &memory)
-	{
-		Byte Data = memory[Address];
-		Cycles--;
-		return Data;
-	}
-	//opcodes
-	static constexpr Byte
-		INS_LDA_IM = 0xA9,
-		INS_LDA_ZP = 0XA5,
-		INS_LDA_ZPX = 0XB5;
-	void LDASetStatus()
-	{
-		Z = (A == 0);
-		N = (A & 0b10000000)> 0;
-	}
+	};
 
-	void Execute(u32 Cycles, Mem &memory)
+	struct CPU
 	{
-		while (Cycles > 0)
+
+		Word PC; //Program Counter
+		Word SP; //Stack pointer
+
+		Byte A, X, Y; // Registers
+
+		Byte C : 1; //Status flag
+		Byte Z : 1;
+		Byte I : 1;
+		Byte D : 1;
+		Byte B : 1;
+		Byte V : 1;
+		Byte N : 1;
+
+		void Reset(Mem &memory)
 		{
-			Byte Ins = FetchByte(Cycles, memory);
-			switch (Ins)
+			PC = 0xFFFC;
+			SP = 0x100;
+			C = Z = I = D = B = V = N = D = 0;
+			A = X = Y = 0;
+			memory.Initialise();
+		}
+
+		Byte FetchByte(u32 &Cycles, Mem &memory)
+		{
+			Byte Data = memory[PC];
+			PC++;
+			Cycles--;
+			return Data;
+		}
+		Word FetchWord(u32 &Cycles, Mem &memory)
+		{
+			//6502 is little endian
+			Word Data = memory[PC];
+			PC++;
+
+			Data |= (memory[PC] << 8);
+			PC++;
+
+			Cycles -= 2;
+
+			//if you wanted to handle endianess
+			//you would have to swap bytes here
+			// IF (PLATFORM_BIG_ENDIAN) swap bytes in Word
+
+			return Data;
+		}
+		Byte ReadByte(u32 &Cycles, Byte Address, Mem &memory)
+		{
+			Byte Data = memory[Address];
+			Cycles--;
+			return Data;
+		}
+		//opcodes
+		static constexpr Byte
+			INS_LDA_IM = 0xA9,
+			INS_LDA_ZP = 0XA5,
+			INS_LDA_ZPX = 0XB5,
+			INS_JSR = 0x20;
+		void LDASetStatus()
+		{
+			Z = (A == 0);
+			N = (A & 0b10000000) > 0;
+		}
+
+		void Execute(u32 Cycles, Mem &memory)
+		{
+			while (Cycles > 0)
 			{
-			case INS_LDA_IM:
-			{
-				Byte Value = FetchByte(Cycles, memory);
-				A = Value;
-				LDASetStatus();
-			}
-			case INS_LDA_ZP:
-			{
-				Byte ZeroPageAddr = FetchByte(Cycles, memory);
-				ReadByte(Cycles,ZeroPageAddr, memory);
-				LDASetStatus();
-			}
-			break;
-			case INS_LDA_ZPX:
-			{
-				Byte ZeroPageAddr = FetchByte(Cycles, memory);
-				ZeroPageAddr +=X;
-				Cycles--;
-				A = ReadByte(Cycles,ZeroPageAddr,memory);
-			}
-			break;
-			default:
-			{
-				printf("Instruction not handled %d", Ins);
-			}
-			break;
+				Byte Ins = FetchByte(Cycles, memory);
+				switch (Ins)
+				{
+				case INS_LDA_IM:
+				{
+					Byte Value = FetchByte(Cycles, memory);
+					A = Value;
+					LDASetStatus();
+				}
+				case INS_LDA_ZP:
+				{
+					Byte ZeroPageAddr = FetchByte(Cycles, memory);
+					ReadByte(Cycles, ZeroPageAddr, memory);
+					LDASetStatus();
+				}
+				break;
+				case INS_LDA_ZPX:
+				{
+					Byte ZeroPageAddr = FetchByte(Cycles, memory);
+					ZeroPageAddr += X;
+					Cycles--;
+					A = ReadByte(Cycles, ZeroPageAddr, memory);
+					LDASetStatus(); //TODO: NO OVERFLOW Control
+				}
+				break;
+				case INS_JSR:
+				{
+					Word SubAddr = FetchWord(Cycles, memory);
+					memory[SP] = PC - 1;
+					Cycles--;
+					PC = SubAddr;
+					Cycles--;
+				}
+				break;
+				default:
+				{
+					printf("Instruction not handled %d", Ins);
+				}
+				break;
+				}
 			}
 		}
-	}
-};
+	};
 
-int main()
-{
-	Mem mem;
-	CPU cpu;
-	cpu.Reset(mem);
-	//inline little program
-	mem[0xFFFC] = CPU::INS_LDA_ZP;
-	mem[0xFFFD] = 0x42;
-	mem[0x0042] = 0x84;
-	//inline little program
-	cpu.Execute(3, mem); //TODO Need to find suitable debugger
-	printf("Program finished");
-	return 0;
-}
+	int main()
+	{
+		Mem mem;
+		CPU cpu;
+		cpu.Reset(mem);
+		//inline little program
+		mem[0xFFFC] = CPU::INS_LDA_ZP;
+		mem[0xFFFD] = 0x42;
+		mem[0x0042] = 0x84;
+		//inline little program
+		cpu.Execute(3, mem); //TODO Need to find suitable debugger
+		printf("Program finished");
+		return 0;
+	}
